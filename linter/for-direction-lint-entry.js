cf7042d9902f9ee556d36859f9e9f166a970d84f
lewiscutey <949124363@qq.com>
2022-05-24 21:40:59 +0800

    add linter

---
+++ linter/for-direction-lint-entry.js
@@ -0,0 +1,25 @@
+const { transformFromAstSync } = require('@babel/core');
+const  parser = require('@babel/parser');
+const forDirectionLintPlugin = require('./plugins/for-direction-lint');
+
+const sourceCode = `
+for (var i = 0; i < 10; i++) {
+}
+for (var i = 10; i >= 0; i--) {
+}
+for (var i = 0; i < 10; i--) {
+}
+for (var i = 10; i >= 0; i++) {
+}
+`;
+
+const ast = parser.parse(sourceCode, {
+    sourceType: 'unambiguous'
+});
+
+const { code } = transformFromAstSync(ast, sourceCode, {
+    plugins: [forDirectionLintPlugin],
+    filename: 'input.js'
+});
+
+console.log(code);
---
+++ linter/no-func-assign-lint-entry.js
@@ -0,0 +1,23 @@
+const { transformFromAstSync } = require('@babel/core');
+const  parser = require('@babel/parser');
+const noFuncAssignLintPlugin = require('./plugins/no-func-assign-lint');
+
+const sourceCode = `
+    function foo() {
+        foo = bar;
+    }
+    var a = function hello() {
+    hello = 123;
+    };
+`;
+
+const ast = parser.parse(sourceCode, {
+    sourceType: 'unambiguous'
+});
+
+const { code } = transformFromAstSync(ast, sourceCode, {
+    plugins: [noFuncAssignLintPlugin],
+    filename: 'input.js'
+});
+
+console.log(code);
